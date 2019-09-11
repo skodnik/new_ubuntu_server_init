bddf9e7869d436f3e7d679e034bdf9fae1fdd93c
vlsv <g.skodnik@gmail.com>
2019-09-11 14:52:46 +0300

    init

--- server-init.sh
+++ server-init.sh
@@ -9,17 +9,29 @@
 #   wget https://raw.githubusercontent.com/skodnik/new_ubuntu_server_init/master/server-init.sh
 #   sh install.sh
 
-RED=$(printf '\033[31m')
-GREEN=$(printf '\033[32m')
-YELLOW=$(printf '\033[33m')
-BLUE=$(printf '\033[34m')
-BOLD=$(printf '\033[1m')
-RESET=$(printf '\033[m')
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
+}
 
+setup_color
 
 cat <<-EOF
 
-    I can't change your shell automatically because this system does not have chsh.
     ${BLUE}Please manually change your default shell to zsh${RESET}
 
 EOF
