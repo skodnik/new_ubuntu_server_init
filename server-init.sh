59f75396afd51a21d522627033bba644c73ffdd7
vlsv <g.skodnik@gmail.com>
2019-09-13 11:58:11 +0300

    init

--- server-init.sh
+++ server-init.sh
@@ -42,11 +42,11 @@ echo "\n${YELLOW}>>>>>>>> new sudo user setting up <<<<<<<<${RESET}\n"
 echo "New sudo user name:"
 read NEW_USER
 adduser --debug ${NEW_USER}
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     exit 1
 fi
 usermod -a -G sudo ${NEW_USER}
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
     echo "${GREEN}User ${NEW_USER} has become sudo!${RESET}"
 else
     echo "${RED}Something wrong with making sudo user!${RESET}"
