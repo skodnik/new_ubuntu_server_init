f7590ba0eb336c9a1f9223485c7ebd388816d8f1
Evgeny Vlasov <vlasov.evgeny@gmail.com>
2021-06-27 16:42:59 +0300

    + docker-compose

--- server-init_ubuntu_2004.sh
+++ server-init_ubuntu_2004.sh
@@ -111,6 +111,10 @@ if [ $DOCKER_INSTALL = 'y' ]; then
   systemctl enable --now docker
   docker --version
   usermod -aG docker ${NEW_USER}
+  curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+  docker-compose --version
 fi
 
 echo "\n${YELLOW}>>>>>>>> nginx and certbot setting up <<<<<<<<${RESET}\n"
